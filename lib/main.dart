fbd8ae953b2bcba8fac8ba0c68ed17b8667bba9a
CodeWithEyeasin <mreyeasinarafat@gmail.com>
2024-07-01 16:29:03 +0600

    new

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class TicTacToeGame extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: SplashScreen() ,
     );
   }
 }
